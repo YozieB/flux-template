dc5a7292a26c338432bbd08fa36fdd69ce0b1526
NikkkkR <nikita-kirillov-2015@mail.ru>
2025-06-22 15:22:38 +0300

    feat(styles)закончил стили

--- src/App.tsx
+++ src/App.tsx
@@ -4,83 +4,104 @@ import React, { FC, useState } from 'react';
 
 import { TemplateProps } from './types';
 
-const beautifyDate = (date: Date) => {
-    const formatter = new Intl.DateTimeFormat('en-US', {
-        month: 'long',
-        year: 'numeric',
-        era: undefined,
-    });
-    const monthYear = formatter.format(new Date(date)).replace(' г.', '');
-
-    return monthYear.charAt(0).toUpperCase() + monthYear.slice(1);
-};
-
 export const Flux: FC<TemplateProps> = ({ firstName, lastName, role, experience, education, socials }) => {
     return (
         <div className=" flex relative justify-center items-center min-h-screen bg-[#181616] text-[#ffff]">
-            <div className="w-[600px]">
-                <p className="text-4xl">{'<HTML>'}</p>
+            <div className="w-[600px] text-[#696969]">
+                <p className="text-4xl ">{'<HTML>'}</p>
                 <div>
-                    <h1 className="text-5xl font-bold mb-2">
+                    <h1 className="text-5xl font-bold mb-2 text-[#3DFFDB]">
                         //{firstName} {lastName}
                     </h1>
                     <div className="flex text-2xl">
                         <p>{'<title>'}</p>
-                        <p>{role}</p>
+                        <p className="text-[#FFFFFF]">{role}</p>
                         <p>{'<title>'}</p>
                     </div>
                     <p className="text-2xl">{'<body>'}</p>
                 </div>
-                <div>
+                <div className="">
                     <div className="mt-5">
-                        <h2 className="text-2xl">{'<EXPIRIENCE>'}</h2>
+                        <h2 className="relative left-7 mb-2 text-2xl first-letter:text-[#696969] text-[#0094FF]">
+                            {'<experience'}
+                            <span className="text-[#696969]">{'>'}</span>
+                        </h2>
                         {experience && (
-                            <ul className="text-base">
+                            <ul className="relative left-16 text-base">
                                 {experience.map((exper) => (
-                                    <li className="mb-" key={exper.companyName + exper.role}>
-                                        <h3 className="">{'<li>' + exper.role + ' | ' + exper.companyName + '</li>'}</h3>
-                                        <p className="">{exper.description && '<p>' + exper.description + '</p>'}</p>
+                                    <li className="mb-5" key={exper.companyName + exper.role}>
+                                        <h3 className="">
+                                            {'<li>'}{' '}
+                                            <span className="text-[#3DFFDB]">
+                                                {exper.role}|{exper.companyName}
+                                            </span>{' '}
+                                            {'</li>'}
+                                        </h3>
+                                        {exper.description && (
+                                            <p className="-indent-8 relative left-8">
+                                                {'<p>'} <span className="text-[#FFFF]">{exper.description}</span>
+                                                {'</p>'}
+                                            </p>
+                                        )}
                                     </li>
                                 ))}
                             </ul>
                         )}
-                        <h2 className="text-2xl">{'<EXPIRIENCE>'}</h2>
+                        <h2 className="relative left-7 bottom-4 text-2xl ">{'<experience>'}</h2>
                     </div>
                     <div className="mt-5">
-                        <h2 className="text-2xl">{'<education>'}</h2>
+                        <h2 className="relative left-7 text-2xl mb-2 first-letter:text-[#696969] text-[#0094FF]">
+                            {'<education'}
+                            <span className="text-[#696969]">{'>'}</span>
+                        </h2>
                         {education && (
-                            <ul className="text-base">
+                            <ul className="relative left-16 text-base">
                                 {education.map((educate, index) => (
                                     <li className="" key={index}>
                                         <div className="flex">
-                                            <h3 className="">{'<li>' + educate.university}</h3>
-                                            <p className="">
+                                            <h3>
+                                                {'<li>'}
+                                                <span className="text-[#3DFFDB]">{educate.university}</span>
+                                            </h3>
+                                            <p className="text-[#3DFFDB]">
                                                 {`(${educate.dates[0].getFullYear()} — ${
                                                     educate.isOngoing ? 'Present' : educate.dates[1].getFullYear()
-                                                }) </li>`}
+                                                })`}
+                                                <span className="text-[#696969]">{'</li>'}</span>
                                             </p>
                                         </div>
-                                        <p className="">{'<p>' + educate.degree + '</p>'}</p>
+                                        <p className="-indent-8 relative left-8">
+                                            {'<p>'}
+                                            <span className="text-[#FFFF]">{educate.degree}</span>
+                                            {'</p>'}
+                                        </p>
                                     </li>
                                 ))}
                             </ul>
                         )}
-                        <h2 className="text-2xl">{'<education>'}</h2>
+                        <h2 className="relative left-7 text-2xl">{'<education>'}</h2>
                     </div>
                     <div className="mt-5">
-                        <h2 className="text-2xl">{'<contact>'}</h2>
+                        <h2 className="relative left-7 text-2xl first-letter:text-[#696969] text-[#0094FF]">
+                            {'<contact'}
+                            <span className="text-[#696969]">{'>'}</span>
+                        </h2>
                         {socials && (
-                            <ul className="text-base relative">
+                            <ul className="text-base relative left-16">
                                 {socials.map((social) => (
                                     <li key={social.name} className="left1-1/4">
                                         <a href={social.link} target="_blank">
-                                            {'<a href=”' + social.link + '”>' + social.name + '</a>'}
+                                            {'<a href=”'}
+                                            <span className="text-[#ffff]">{social.link}</span>
+                                            {'”>'}
+                                            <span className="text-[#3DFFDB]">{social.name}</span>
+                                            {'</a>'}
                                         </a>
                                     </li>
                                 ))}
                             </ul>
                         )}
-                        <h2 className="text-2xl">{'<contact>'}</h2>
+                        <h2 className="relative left-7 text-2xl">{'<contact>'}</h2>
                     </div>
                 </div>
             </div>
