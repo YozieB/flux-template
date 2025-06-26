34bf9ddd1d6cf3ea3c012d71c96ebb0371f06103
NikkkkR <nikita-kirillov-2015@mail.ru>
2025-06-26 09:38:03 +0300

    feat(todo) сделал todoшки

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,77 @@ import { TemplateProps } from './types';
 import { Tag } from '@/components/tag';
 import { Section } from '@/components/section';
 
-// TODO: Вынести все теги в компонент Tag
-// TODO: Добавить закрывающие body, html
-// TODO: цвета в переменные
-// TODO: убрать везде relative и left, использовать паддинги
-// TODO: добавить блок с языками, name - li, level - p
-// TODO: добавить блок со skills, все лишками делаем
+// TODO: Вынести все теги в компонент Tag d
+// TODO: Добавить закрывающие body, html d
+// TODO: цвета в переменные d
+// TODO: убрать везде relative и left, использовать паддинги d
+// TODO: добавить блок с языками, name - li, level - p d
+// TODO: добавить блок со skills, все лишками делаем d
 // TODO: добавить блок с hobbies, все лишками делаем
-// TODO: добавить даты в experience блок (только года)
-// TODO: использовать для всех блоков компонент Section
+// TODO: добавить даты в experience блок (только года) d
+// TODO: использовать для всех блоков компонент Section d
 
-export const Flux: FC<TemplateProps> = ({ firstName, lastName, role, experience, education, socials }) => {
+export const Flux: FC<TemplateProps> = ({ firstName, lastName, role, experience, education, socials, skills, languages, hobbies }) => {
     return (
         <div className="flex relative justify-center items-center min-h-screen bg-[#181616] py-24">
             <div className="w-[650px] text-default">
-                <p className="text-4xl">{'<HTML>'}</p>
-                <div>
-                    <h1 className="text-5xl font-bold mb-2 text-title">
-                        //{firstName} {lastName}
-                    </h1>
-                    <div className="flex text-2xl">
-                        <Tag tag="p">title</Tag>
-                        <p className="text-white">{role}</p>
-                        <Tag tag="p">/title</Tag>
-                    </div>
-                    <Tag tag="p" wrapperClassNames="text-2xl">
-                        body
-                    </Tag>
+                <Tag tag="p" wrapperClassNames="text-4xl">
+                    HTML
+                </Tag>
+                <h1 className="text-5xl font-bold mb-2 text-title">
+                    //{firstName} {lastName}
+                </h1>
+                <div className="flex text-2xl">
+                    <Tag tag="p">title</Tag>
+                    <p className="text-white">{role}</p>
+                    <Tag tag="p">/title</Tag>
                 </div>
+                <Tag tag="p" wrapperClassNames="text-2xl">
+                    body
+                </Tag>
+                {!!skills?.length && (
+                    <Section haveGap={false} title="skills">
+                        <div className="grid grid-cols-2">
+                            {skills.map((skill) => (
+                                <li key={skill} className="flex">
+                                    <Tag tag="p">li</Tag>
+                                    <p className="text-title">
+                                        {skill}
+                                        <Tag tag="span" wrapperClassNames="text-default">
+                                            /li
+                                        </Tag>
+                                    </p>
+                                </li>
+                            ))}
+                        </div>
+                    </Section>
+                )}
+                {!!languages?.length && (
+                    <Section haveGap={false} title="languages">
+                        {languages.map((language) => (
+                            <li className="flex">
+                                <div className="flex">
+                                    <Tag tag="p">li</Tag>
+                                    <p className="text-title">
+                                        {language.name}
+                                        <Tag tag="span" wrapperClassNames="text-default">
+                                            /li
+                                        </Tag>
+                                    </p>
+                                </div>
+                                <div className="flex">
+                                    <Tag tag="p">p</Tag>
+                                    <p className="text-white">
+                                        {language.level}
+                                        <Tag tag="span" wrapperClassNames="text-default">
+                                            /p
+                                        </Tag>
+                                    </p>
+                                </div>
+                            </li>
+                        ))}
+                    </Section>
+                )}
                 {!!experience?.length && (
                     <Section title="experience">
                         {experience.map((exper) => (
@@ -42,6 +85,7 @@ export const Flux: FC<TemplateProps> = ({ firstName, lastName, role, experience,
                                     <Tag tag="span">li</Tag>
                                     <span className="text-title">
                                         {exper.role} | {exper.companyName}
+                                        {' ( ' + exper.dates[0].getFullYear() + '-' + exper.dates[1].getFullYear() + ' )'}
                                     </span>
                                     <Tag tag="p">/li</Tag>
                                 </div>
@@ -60,60 +104,68 @@ export const Flux: FC<TemplateProps> = ({ firstName, lastName, role, experience,
                         ))}
                     </Section>
                 )}
-                <div className="mt-5">
-                    <h2 className="relative left-7 text-2xl mb-2 first-letter:text-[#696969] text-[#0094FF]">
-                        {'<education'}
-                        <span className="text-[#696969]">{'>'}</span>
-                    </h2>
-                    {education && (
-                        <ul className="relative left-16 text-base">
-                            {education.map((educate, index) => (
-                                <li className="" key={index}>
-                                    <div className="flex">
-                                        <h3>
-                                            {'<li>'}
-                                            <span className="text-[#3DFFDB]">{educate.university}</span>
-                                        </h3>
-                                        <p className="text-[#3DFFDB]">
-                                            {`(${educate.dates[0].getFullYear()} — ${
-                                                educate.isOngoing ? 'Present' : educate.dates[1].getFullYear()
-                                            })`}
-                                            <span className="text-[#696969]">{'</li>'}</span>
-                                        </p>
-                                    </div>
-                                    <p className="-indent-8 relative left-8">
-                                        {'<p>'}
-                                        <span className="text-[#FFFF]">{educate.degree}</span>
-                                        {'</p>'}
+                {!!education?.length && (
+                    <Section title="education">
+                        {education.map((educate, index) => (
+                            <li className="" key={index}>
+                                <div className="flex">
+                                    <Tag tag="span">li</Tag>
+                                    <p className="text-title">
+                                        {educate.university}
+                                        {` (${educate.dates[0].getFullYear()} — ${
+                                            educate.isOngoing ? 'Present' : educate.dates[1].getFullYear()
+                                        })`}
+                                        <Tag tag="span" wrapperClassNames="text-default">
+                                            /li
+                                        </Tag>
                                     </p>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                    <h2 className="relative left-7 text-2xl">{'<education>'}</h2>
-                </div>
-                <div className="mt-5">
-                    <h2 className="relative left-7 text-2xl first-letter:text-[#696969] text-[#0094FF]">
-                        {'<contact'}
-                        <span className="text-[#696969]">{'>'}</span>
-                    </h2>
-                    {socials && (
-                        <ul className="text-base relative left-16">
-                            {socials.map((social) => (
-                                <li key={social.name} className="left1-1/4">
-                                    <a href={social.link} target="_blank">
-                                        {'<a href=”'}
-                                        <span className="text-[#ffff]">{social.link}</span>
-                                        {'”>'}
-                                        <span className="text-[#3DFFDB]">{social.name}</span>
-                                        {'</a>'}
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                    <h2 className="relative left-7 text-2xl">{'<contact>'}</h2>
-                </div>
+                                </div>
+                                <p className="text-white">
+                                    <Tag tag="span" wrapperClassNames="text-default">
+                                        p
+                                    </Tag>
+                                    {educate.degree}
+                                    <Tag tag="span" wrapperClassNames="text-default">
+                                        /p
+                                    </Tag>
+                                </p>
+                            </li>
+                        ))}
+                    </Section>
+                )}
+                {!!hobbies?.length && (
+                    <Section haveGap={false} title="hobbies">
+                        {hobbies.map((hobbie) => (
+                            <li className="flex">
+                                <Tag tag="p">li</Tag>
+                                <p className="text-white">{hobbie}</p>
+                                <Tag tag="p">/li</Tag>
+                            </li>
+                        ))}
+                    </Section>
+                )}
+                {!!socials?.length && (
+                    <Section title="contact" haveGap={false}>
+                        {socials.map((social) => (
+                            <li key={social.name} className="">
+                                <a href={social.link} target="_blank">
+                                    {'<a href=”'}
+                                    <span className="text-white">{social.link}</span>
+                                    {'”>'}
+                                    <span className="text-title">{social.name}</span>
+                                    {'</a>'}
+                                </a>
+                            </li>
+                        ))}
+                    </Section>
+                )}
+
+                <Tag tag="p" wrapperClassNames="text-2xl pt-4">
+                    body
+                </Tag>
+                <Tag tag="p" wrapperClassNames="text-4xl">
+                    HTML
+                </Tag>
             </div>
         </div>
     );
